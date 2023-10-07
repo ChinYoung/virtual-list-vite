878bf5101283de974d934328567803b8d0495672
yang qin <woodichin@outlook.com>
2023-10-07 16:37:22 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-gray-500'>
+      </div>
+      <h1 className='text-blue-500'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
